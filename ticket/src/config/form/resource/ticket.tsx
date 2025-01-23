3f77be2c6ecd3e7ee7b03b6d51f11f927bd64593
Vijay-mehta <infovijay1998@gmail.com>
2025-01-23 22:38:55 +0530

    initial commit

---
+++ ticket/src/config/form/resource/ticket.tsx
@@ -0,0 +1,24 @@
+const ticket =[
+    {
+        title:"Title",
+        column:"title",
+        type:"text"
+    },
+    {
+        title:"Description",
+        column:"description",
+        type:"textarea"
+    },
+    {
+        title:"Date",
+        column:"date",
+        type:"date"
+    },
+    {
+        title:"Priority",
+        column:"date",
+        type:"checkbox"
+    }
+]
+
+export default ticket;
